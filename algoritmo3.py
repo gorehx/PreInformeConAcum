653670be30a1e7ce134e1f57710d659f62be4eff
Maria Fernanda Alvarez Aguirre <000405195@bga.upb>
2019-03-08 11:22:44 -0500

    Primer commit

---
+++ algoritmo3.py
@@ -0,0 +1,17 @@
+#Un algoritmo que lea 5 números e imprima el mayor número leído.
+print('El mayor de cinco números')
+a=float(input('Ingrese el primer número'))
+b=float(input('Ingrese el segundo número'))
+c=float(input('Ingrese el tercer número'))
+d=float(input('Ingrese el cuarto número'))
+e=float(input('Ingrese el quinto número'))
+if (a>b and a>c and a>d and a>e):
+   print('El mayor es', a)
+if (b>a and b>c and b>d and b>e):
+   print('El mayor es', b)
+if (c>a and c>b and c>d and c>e):
+   print('El mayor es', a)
+if (d>a and d>b and d>c and d>e):
+   print('El mayor es', a)
+if (e>a and e>b and e>c and e>d):
+   print('El mayor es', a)
